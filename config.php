48936247483414cf46cb5a2cb1180a723d8945f6
alan bount <alan@zeroasterisk.com>
2013-03-13 17:17:21 -0400

    external config file support, outside of git
    altered config.php to allow for an external config file
    or we could setup some alternative solution for this... (copy default file?)
    
    but as it is now, this file is a part of git,
    and if someone changes it to their values, their git instance is either forked or non-committed
    which means they can not get future updates easily.

--- config.php
+++ config.php
@@ -1,3 +1,18 @@
 <?php
-define('UC_PUBLIC_KEY', 'demopublickey');
-define('UC_SECRET_KEY', 'demoprivatekey');
+/**
+ * Configure the API keys
+ *
+ * you may optionally create a configuration file and put it outside of the
+ * uploadcare folder, which will make future upgrading easier
+ */
+@include dirname(dirname(__FILE__)) . 'uploadcare_config.php';
+
+/**
+ * If not defined yet, please go ahead and enter your public and secret
+ */
+if (!defined('UC_PUBLIC_KEY')) {
+	define('UC_PUBLIC_KEY', 'demopublickey');
+}
+if (!defined('UC_SECRET_KEY')) {
+	define('UC_SECRET_KEY', 'demoprivatekey');
+}
